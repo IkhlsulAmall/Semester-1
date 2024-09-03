8eba4c3461697500050ef816c05bb0ace0955be2
Muh. Ikhlasul Amal. N <147160150+Ikhlasull@users.noreply.github.com>
2024-09-04 07:29:24 +0800

    Add files via upload
    Versi Upgrade dari Codingan Berat Badan

---
+++ Modul 1 - Pengkondisian If Else/VersiUpgradeBeratBadan.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float BMI,berat,tinggi,tinggi2;
+    char ulang;
+    do {
+    cout << "Masukkan tinggi badan (cm) : ";cin >> tinggi;
+    // untuk memeriksa input valid
+    if(cin.fail()){
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Input tidak valid, Masukkan angka.\n";
+        continue;
+    }
+
+    while(true){ // Untuk memeriksa input valid atau tidak
+    cout << "Masukkan berat badan (kg) : ";cin >> berat;
+    if(cin.fail()){
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Input tidak valid, Masukkan angka.\n";
+    }else{
+        break;
+    }
+    }
+    tinggi2 = tinggi / 100;
+    BMI = (berat/(tinggi2*tinggi2)); 
+    cout << "\nHasil:";
+    cout << "\nBMI        = " << BMI;  
+
+    if(BMI < 18.5){
+        cout << "\nKategori   = Berat badan kurang\n";
+    } else if(BMI <= 22.9 && BMI >= 18.5){
+        cout << "\nKategori   = Berat badan normal\n";
+    } else if(BMI <= 29.9 && BMI >= 23){
+        cout << "\nKategori   = Berat badan berlebih\n";
+    }else{
+        cout << "\nKategori   = Obesitas\n";
+    }
+    cout << "\n=====================================";
+    cout << "\nApakah anda ingin menghitung ulang? \nPilih 'Y' atau 'N' : ";cin >> ulang;
+    cout << "=====================================\n"; 
+    cin.ignore(1000, '\n'); // Ini berfungsi untuk menghapus buffer atau inputan yang delay
+    } while (ulang == 'y' || ulang == 'Y'); //disini untung mengulang program
+    return 0;                               //ketika while bersifat true atau bernilai Y
+}
